2a50e17ba65a8cb92a0e0446649e3e608a76c90f
Victor Hugo Moreira <vhmrj92@gmail.com>
2023-11-02 22:43:09 -0300

    features(componentes): criação de componente de homepage

--- frontend-todoist/src/app/app.module.ts
+++ frontend-todoist/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
